146b1a6574e15a4f771c88b34bdbaba6b06c6eca
cssf998811 <cssf998811@gmail.com>
2024-05-01 18:07:58 +0800

    add Job model

--- routes/web.php
+++ routes/web.php
@@ -1,38 +1,20 @@
 <?php
 
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
-
-$jobs = [
-    [
-        'id' => 1,
-        'title' => 'Director',
-        'salary' => '$50,000'
-    ],
-    [
-        'id' => 2,
-        'title' => 'Programmer',
-        'salary' => '$10,000'
-    ],
-    [
-        'id' => 3,
-        'title' => 'Teacher',
-        'salary' => '$40,000'
-    ]
-];
+use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function () use ($jobs) {
+Route::get('/jobs', function () {
     return view('jobs', [
-        'jobs' => $jobs
+        'jobs' => Job::all()
     ]);
 });
 
-Route::get('/jobs/{id}', function ($id) use ($jobs) {
-    $job = Arr::first($jobs, fn($job) => $job['id']== $id);
+Route::get('/jobs/{id}', function ($id)  {
+    $job = Job::find($id);
     return view('job', ['job' => $job]);
 });
 
