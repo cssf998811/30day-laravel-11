a35a42c168e04a63080f2403ce010a01c8c4919e
cssf998811 <cssf998811@gmail.com>
2024-06-12 18:25:37 +0800

    Day15: Understanding Database Seeders

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,7 +30,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
-            'admin' => false
+//            'admin' => false
         ];
     }
 
@@ -45,10 +45,10 @@ class UserFactory extends Factory
     }
 
     // User::factory()->admin()->create()
-    public function admin(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'admin' => true,
-        ]);
-    }
+//    public function admin(): static
+//    {
+//        return $this->state(fn (array $attributes) => [
+//            'admin' => true,
+//        ]);
+//    }
 }
